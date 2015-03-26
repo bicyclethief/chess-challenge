0b9b145318c07325150064e4b07f79da213e2be4
bicyclethief & Lucian Kahn <pair+bicyclethief.luciankahn@devbootcamp.com>
2015-03-26 15:49:29 -0400

    create all_moves for King class

--- king.rb
+++ king.rb
@@ -1,6 +1,6 @@
 class King < Piece
 
-  # legal_moves(Square, Board): returns Array of Squares of legal moves
+  # returns Array of Squares of legal moves
   def legal_moves(square_object, board_object)
     array_of_squares = []
 
@@ -14,14 +14,19 @@ class King < Piece
 
   private
 
-  # returns Array of possible Squares given the Piece's move pattern
+  # returns Array of all possible Squares given the Piece's move pattern
   def all_moves(square_object)
-    array_of_squares = []
+    moves = []
 
+    moves << (square_object.row += 1) # forward 1 square
+    moves << (square_object.row -= 1) # backward 1 square
 
+    my_coordinate = to_grid_notation(square_object) #
 
+    moves << to_chess_notation(my_coordinate.column += 1) # right 1 square
+    moves << to_chess_notation(my_coordinate.column -= 1) # left 1 square
 
-    array_of_squares
+    moves
   end
 
 
