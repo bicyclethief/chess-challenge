d987c092defa968d776d9fe4b2efc7d29109ff48
Vinh Bui <bicyclethief@gmail.com>
2015-03-26 19:30:32 -0400

    committing and pushing for a progress update per Lucian

---
+++ coordinate.rb
@@ -0,0 +1,10 @@
+
+class Coordinate
+
+  attr_accessor :column, :row
+
+  def initialize(row = nil, column = nil)
+    @column = column
+    @row = row
+  end
+end
---
+++ notation.rb
@@ -0,0 +1,16 @@
+require_relative 'square'
+require_relative 'coordinate'
+
+module Notation
+
+  LETTER_INDEX = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7 }
+  LETTERS = ('a'..'h').to_a.reverse
+
+  def to_chess_notation(coordinate)
+    Square.new(coordinate.row + 1, LETTERS[coordinate.column])
+  end
+
+  def to_grid_notation(square)
+    Coordinate.new(LETTER_INDEX[square.column], square.row - 1, )
+  end
+end
