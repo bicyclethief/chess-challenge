5c7205a7cc086dc31451b66123d04845cd1805f2
bicyclethief & Lucian Kahn <pair+bicyclethief.luciankahn@devbootcamp.com>
2015-03-26 16:17:11 -0400

    legal_moves for King class

--- king.rb
+++ king.rb
@@ -1,34 +1,41 @@
 class King < Piece
 
   # returns Array of Squares of legal moves
-  def legal_moves(square_object, board_object)
-    array_of_squares = []
-
-
-
+  def legal_moves(square_object, board)
+    array_of_squares = all_moves(square_object)
+    array_of_squares.each do |square|
+      content = board.get_square_content(square)
+      if content != nil                       # if it's not empty
+        if content.color == self.color        # if it's the same color as my King
+          array_of_squares.delete(square)
+        end
+      end
+    end
     array_of_squares
   end
 
   def to_s
+
   end
 
   private
 
   # returns Array of all possible Squares given the Piece's move pattern
   def all_moves(square_object)
-    moves = []
+    all = []
 
-    moves << (square_object.row += 1) # forward 1 square
-    moves << (square_object.row -= 1) # backward 1 square
+    all << (square_object.add_row(1)) # forward 1 square
+    all << (square_object.add_row(-1)) # backward 1 square
 
-    my_coordinate = to_grid_notation(square_object) #
+    # this is the code for lines 35-36 if Square doesn't handle its own columns
+    # my_coordinate = to_grid_notation(square_object)
+    # all << to_chess_notation(my_coordinate.column += 1) # right 1 square
+    # all << to_chess_notation(my_coordinate.column -= 1) # left 1 square
 
-    moves << to_chess_notation(my_coordinate.column += 1) # right 1 square
-    moves << to_chess_notation(my_coordinate.column -= 1) # left 1 square
+    all << (square_object.add_column(1)) # right 1 square
+    all << (square_object.add_column(-1)) # left 1 square
 
-    moves
+    all
   end
-
-
 
 end
