6178cd53d1f48f96d01ea9e1fcf520a740394696
Lucian Kahn <luciankahn@gmail.com>
2015-03-27 17:57:54 -0400

    legal squares is broken in Rook

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -31,6 +31,26 @@ describe "Rook" do
 
   it "should consider moving 7 forward, 7 backward, 7 left, or 7 right valid within bounds
   valid on an empty board" do
+    board = Board.new
+    h1 = Square.new('h', 1)
+    h8 = Square.new('h', 8)
+    a1 = Square.new('a', 1)
+    a8 = Square.new('a', 8)
+
+    white_rook = Rook.new(Piece::COLOR_WHITE)
+    board.place_piece(h1, white_rook)
+
+    h8match = white_rook.legal_moves(h1, board).any? {|square| square.equal?(h8)}
+    expect(h8match).to eq(true)
+    a1match = white_rook.legal_moves(h1, board).any? {|square| square.equal?(a1)}
+    expect(a1match).to eq(true)
+  end
+
+  it "should consider a move invalid if it would land on a piece of the same color" do
+
+  end
+
+  it "should consider a move valid if it would land on an opponent piece" do
 
 
   end
