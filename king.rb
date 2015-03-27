10eb4d8aa250e5c2198c4fc555f54451aeba5bab
Lucian Kahn <luciankahn@gmail.com>
2015-03-27 10:40:31 -0400

    about to add diagonals for king

--- king.rb
+++ king.rb
@@ -1,11 +1,6 @@
 require_relative 'piece'
-require_relative 'board'
-require_relative 'coordinate'
-require_relative 'notation'
-require_relative 'game'
-require_relative 'rook'
 require_relative 'square'
-require_relative 'bishop'
+require_relative 'notation'
 
 class King < Piece
 
