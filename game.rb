49a5901a226cbe36704a0d2bd4ed3cc10f64ca0d
Lucian Kahn <luciankahn@gmail.com>
2015-03-26 20:00:33 -0400

    set_all_pieces

--- game.rb
+++ game.rb
@@ -10,8 +10,8 @@ class Game
     @player1 = args[:player1]
     @player2 = args[:player2]
     @colors = [:white, :black].shuffle!
-    @players = {@player1 => @colors[0],
-                @player2 => @colors[1]
+    @players = {@colors[0] => @player1,
+                @colors[1] => @player2
                 }
 
     @board = args[:board]
@@ -19,7 +19,39 @@ class Game
 
   # creates and places pieces on board for a standard game of Chess
   def set_all_pieces(pieces)
-    @board.set_piece("A1", Rook.new())
+    @board.set_piece("A1", Rook.new(:white))
+    @board.set_piece("B1", Knight.new(:white))
+    @board.set_piece("C1", Bishop.new(:white))
+    @board.set_piece("D1", Queen.new(:white))
+    @board.set_piece("E1", King.new(:white))
+    @board.set_piece("F1", Bishop.new(:white))
+    @board.set_piece("G1", Knight.new(:white))
+    @board.set_piece("H1", Rook.new(:white))
+    @board.set_piece("A2", Pawn.new(:white))
+    @board.set_piece("B2", Pawn.new(:white))
+    @board.set_piece("C2", Pawn.new(:white))
+    @board.set_piece("D2", Pawn.new(:white))
+    @board.set_piece("E2", Pawn.new(:white))
+    @board.set_piece("F2", Pawn.new(:white))
+    @board.set_piece("G2", Pawn.new(:white))
+    @board.set_piece("H2", Pawn.new(:white))
+
+    @board.set_piece("A8", Rook.new(:black))
+    @board.set_piece("B8", Knight.new(:black))
+    @board.set_piece("C8", Bishop.new(:black))
+    @board.set_piece("D8", Queen.new(:black))
+    @board.set_piece("E8", King.new(:black))
+    @board.set_piece("F8", Bishop.new(:black))
+    @board.set_piece("G8", Knight.new(:black))
+    @board.set_piece("H8", Rook.new(:black))
+    @board.set_piece("A7", Pawn.new(:black))
+    @board.set_piece("B7", Pawn.new(:black))
+    @board.set_piece("C7", Pawn.new(:black))
+    @board.set_piece("D7", Pawn.new(:black))
+    @board.set_piece("E7", Pawn.new(:black))
+    @board.set_piece("F7", Pawn.new(:black))
+    @board.set_piece("G7", Pawn.new(:black))
+    @board.set_piece("H7", Pawn.new(:black))
   end
 
   def player_turn(player)
