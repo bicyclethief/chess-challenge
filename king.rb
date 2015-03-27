26f99a5cb8af9d4b1513a72ee5fb61b2062aef22
Lucian Kahn <luciankahn@gmail.com>
2015-03-27 10:49:33 -0400

    add diagonals to king

--- king.rb
+++ king.rb
@@ -7,17 +7,17 @@ class King < Piece
   # returns Array of Squares of legal moves
   def legal_moves(square_object, board)
     all = []
+
+    # 90 degrees
     all << (square_object.dup.add_row(1)) # forward 1 square
     all << (square_object.dup.add_row(-1)) # backward 1 square
-    # below is the code if Square doesn't handle its own columns
-    # my_coordinate = to_grid_notation(square_object)
-    # all << to_chess_notation(my_coordinate.column += 1) # right 1 square
-    # all << to_chess_notation(my_coordinate.column -= 1) # left 1 square
     all << (square_object.dup.add_column(1)) # right 1 square
     all << (square_object.dup.add_column(-1)) # left 1 square
-
-    # ADD THE DIAGONALS
-
+    # diagonals
+    all << (square_object.dup.add_row(1).add_column(1))
+    all << (square_object.dup.add_row(-1).add_column(-1))
+    all << (square_object.dup.add_row(-1).add_column(1))
+    all << (square_object.dup.add_row(1).add_column(-1))
 
     array_of_squares = legal_moves_delete(all, board)
   end
