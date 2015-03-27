594f225515acbd91574f9b2b2d407383a4ba2b65
Lucian Kahn <luciankahn@gmail.com>
2015-03-27 14:19:41 -0400

    all it should statements

---
+++ spec/bishop_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../bishop'
+require_relative '../piece'
+
+describe "Bishop" do
+
+  it "should consider out-of-bounds moves invalid" do
+
+  end
+
+  it "should consider all diagonal moves valid within bounds on an empty board" do
+
+  end
+
+  it "should consider a move invalid if it would land on a piece of the same color" do
+
+  end
+
+  it "should consider a move valid if it would land on an opponent piece" do
+
+  end
+
+  it "should consider a move invalid if any pieces are blocking its path to the destination square" do
+
+  end
+
+end
