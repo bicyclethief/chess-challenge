60856cbd2edc56ca93dd7f32337a22343f372be0
kennygeiler <kennethlgeiler@gmail.com>
2015-03-27 12:29:05 -0400

    bishop piece done, hopefully

--- bishop.rb
+++ bishop.rb
@@ -6,38 +6,41 @@ require_relative 'square'
 class Bishop < Piece
 
   def legal_moves(square_object, board)
-    legal_arr = []
-    diagonal_up_right = []
-    diagonal_up_left = []
-    diagonal_down_right= []
-    diagonal_down_left = []
-    clone_obj = square_object.dup
-    until ! (board.empty?(clone_obj)) || board.out_of_bounds(clone_obj)
-      # need to add some sort of code to distinguish between empty squaure vs filled but which color
-      clone_obj.add_row(1)
-      clone_obj.add_column(1)
-      diagonal_up_right << (clone_obj)
+    left_up_array = []
+    left_down_array = []
+    right_up_array = []
+    right_down_array = []
+    while times <= 7
+      diag_square_up = square_object.dup
+      diag_square_up.add_row(1)
+      diag_square_down = square_object.dup
+      diag_square_down.add_row(-1)
+      left_up_array << (diag_square_up.add_column(1))
+      left_down_array<< (diag_square_down.add_column(1))
+      right_up_array << (diag_square_up.add_column(-1))
+      right_down_array << (diag_square_down.add_column(-1))
+      times += 1
     end
-    clone_obj_two = square_object.dup
-    until ! (board.empty?(clone_obj_two)) || board.out_of_bounds(clone_obj_two)
-      clone_obj_two.add_row(1)
-      clone_obj_two.add_column(-1)
-      diagonal_up_left << (clone_obj_two)
-    end
-    clone_obj_three = square_object.dup
-    until ! (board.empty?(clone_obj_three)) || board.out_of_bounds(clone_obj_three)
-      clone_obj_three.add_row(-1)
-      clone_obj_three.add_column(1)
-      diagonal_down_right << (clone_obj_three)
-    end
-    clone_obj_four = square_object.dup
-    until ! (board.empty?(clone_obj_four)) || board.out_of_bounds(clone_obj_four)
-      clone_obj_four.add_row(-1)
-      clone_obj_four.add_column(-1)
-      diagonal_down_left << (clone_obj_four.dup)
-    end
+    left_up_array = legal_moves_delete(forward_array, board)
+    left_down_array = legal_moves_delete(backward_array, board)
+    right_up_array = legal_moves_delete(left_array, board)
+    right_down_array = legal_moves_delete(right_array, board)
 
-    legal_arr <<  diagonal_up_right + diagonal_up_left + diagonal_down_right + diagonal_down_left
+    array_of_squares << left_up_array + left_down_array + right_down_array + right_up_array
+  end
+
+  # helper for legal_moves
+  def legal_moves_delete(direction_array, board, direction)
+
+    content = board.get_square_content(square)
+
+    direction_array.each_with_index do |square|
+      if board.out_of_bounds?(square) || (content != nil && content.color == self.color)
+        array_of_squares.delete(square)
+      end
+      direction_array = direction_array.slice!((index+1)..-1)
+    end
+    direction_array
   end
 
   def to_s
