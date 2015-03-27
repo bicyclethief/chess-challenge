4e28c43dcbac518d7ed8994b506566c543830634
Lucian Kahn <luciankahn@gmail.com>
2015-03-27 12:25:16 -0400

    create knight piece

--- knight.rb
+++ knight.rb
@@ -7,20 +7,24 @@ class Knight < Piece
 
   # returns Array of Squares of legal moves
   def legal_moves(square_object, board)
-
-
-    legal_moves_delete(array_of_squares, board, )
+    array_of_squares << (square_object.dup.add_row(2).add_column(-1))                    # up 2, left 1
+    array_of_squares << (square_object.dup.add_row(2).add_column(1))                     # up 2, right 1
+    array_of_squares << (square_object.dup.add_column(2).add_row(1))                     # right 2, up 1
+    array_of_squares << (square_object.dup.add_column(2).add_row(-1))                    # right 2, down 1
+    array_of_squares << (square_object.dup.add_row(-2).add_column(1))                    # down 2, right 1
+    array_of_squares << (square_object.dup.add_row(-2).add_column(-1))                   # down 2, left 1
+    array_of_squares << (square_object.dup.add_column(-2).add_row(1))                    # left 2, up 1
+    array_of_squares << (square_object.dup.add_column(-2).add_row(-1))                   # left 2, down 1
+    legal_moves_delete(array_of_squares, board)
     array_of_squares
   end
 
   # helper for legal_moves
   def legal_moves_delete(array_of_squares, board)
-
     content = board.get_square_content(square)
-
     array_of_squares.each do |square|
       if board.out_of_bounds?(square) || (content != nil && !self.opponent?(content))
-        array_of_squares.delete(square) # delete it if: out of bounds or it's occupied and the same color as my Rook
+        array_of_squares.delete(square) # delete it if: out of bounds or it's occupied and the same color as my Knight
       end
     end
     array_of_squares
