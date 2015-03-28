797076c1ee50a96752db3d120c531ba07e1b93e4
Lucian Kahn <luciankahn@gmail.com>
2015-03-28 12:22:14 -0400

    fix last commit

--- rook.rb
+++ rook.rb
@@ -14,6 +14,7 @@ class Rook < Piece
     counter = 1
     while counter <= 7
       forward_array << (square_object.dup.add_row(counter)) # forward 1 more square
+      puts "Forward: #{forward_array}"
       right_array << (square_object.dup.add_column(counter)) # right 1 more square
       backward_array << (square_object.dup.add_row(-counter)) # backward 1 square
       left_array << (square_object.dup.add_column(-counter)) # left 1 square
@@ -21,9 +22,13 @@ class Rook < Piece
     end
 
     forward_array = legal_moves_delete(forward_array, board)
+    puts "Forward: #{forward_array}"
     backward_array = legal_moves_delete(backward_array, board)
+    puts "Backward: #{backward_array}"
     left_array = legal_moves_delete(left_array, board)
+    puts "Left: #{left_array}"
     right_array = legal_moves_delete(right_array, board)
+    puts "Right: #{right_array}"
 
     array_of_squares = forward_array + backward_array + left_array + right_array
   end
