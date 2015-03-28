db12eb7a25a03ab08ac1fed6f3ee775a8ed0cd0b
Lucian Kahn <luciankahn@gmail.com>
2015-03-28 12:23:50 -0400

    passing four rspec tests

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -71,7 +71,19 @@ describe "Rook" do
   end
 
   it "should consider a move valid if it would land on an opponent piece" do
+    board = Board.new
+    h1 = Square.new('h', 1)
+    h8 = Square.new('h', 8)
+    a1 = Square.new('a', 1)
+    a8 = Square.new('a', 8)
 
+    white_rook = Rook.new(Piece::COLOR_WHITE)
+    board.place_piece(h1, white_rook)
+    black_king = King.new(Piece::COLOR_BLACK)
+    board.place_piece(h8, black_king)
+
+    h8match = white_rook.legal_moves(h1, board).any? {|square| square.equal?(h8)}
+    expect(h8match).to eq(true)
 
   end
 
