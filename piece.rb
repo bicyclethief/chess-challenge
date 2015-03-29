2a85b6006b456ad5066c38f536a10c9d39feeb2e
Lucian Kahn <luciankahn@gmail.com>
2015-03-29 10:21:19 -0400

    update rook

--- piece.rb
+++ piece.rb
@@ -63,6 +63,29 @@ class Piece
     direction_array
   end
 
+  # implemented by Rook and Queen
+  def cross_checker(square_object, board, counter_max)
+    forward_array = []
+    backward_array = []
+    left_array = []
+    right_array = []
+    counter = 1
+    while counter <= counter_max
+      forward_array << (square_object.dup.add_row(counter)) # forward 1 more square
+      right_array << (square_object.dup.add_column(counter)) # right 1 more square
+      backward_array << (square_object.dup.add_row(-counter)) # backward 1 square
+      left_array << (square_object.dup.add_column(-counter)) # left 1 square
+      counter += 1
+    end
+
+    forward_array = legal_moves_delete(forward_array, board)
+    backward_array = legal_moves_delete(backward_array, board)
+    left_array = legal_moves_delete(left_array, board)
+    right_array = legal_moves_delete(right_array, board)
+
+    forward_array + backward_array + left_array + right_array
+  end
+
   # implemented by Bishop and Queen
   def diagonals_checker(square_object, board, counter_max)
     diag1 = []
