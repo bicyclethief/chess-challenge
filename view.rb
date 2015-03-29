ec3d1dcc66e991d70ec1ad37a0e3b4be499859f5
Vinh Bui <bicyclethief@gmail.com>
2015-03-28 20:00:21 -0400

    loop #player_turn call forever for now

---
+++ view.rb
@@ -0,0 +1,25 @@
+
+class View
+  def turn(player)
+    puts "#{player}'s turn."
+    print "#{player}, your move? "
+  end
+
+  def moves_for(color, piece, square, moves)
+    puts "moves for #{color} #{piece} #{square}: #{moves}"
+  end
+
+  def where(player, square)
+    print "#{player}, move #{square} where? "
+  end
+
+  def moved(player, piece, origin, destination, checkmate = nil)
+    checkmate_txt = checkmate ? " Checkmate." : ""
+    puts "\nOk, #{player}'s #{piece} #{origin} to #{destination}.#{checkmate_txt}"
+  end
+
+  def captures(player1, piece1, origin, player2, piece2, destination, checkmate = nil)
+    checkmate_txt = checkmate ? " Checkmate." : ""
+    puts "\nOk, #{player1}'s #{piece} #{origin} captures #{player1}'s #{piece2} #{destination}.#{checkmate}"
+  end
+end
